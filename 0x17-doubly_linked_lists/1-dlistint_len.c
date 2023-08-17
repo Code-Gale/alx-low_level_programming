1b3bb10ece26a8d46a0c7118fb8011f666bbefae
Code-Gale <codewhiz001@gmail.com>
2023-08-17 17:58:44 +0100

    the fix of dlistint-len

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,16 +1,30 @@
 #include "lists.h"
 
 /**
- * dlistint_len - count the number of nodes in the linked list
+ * dlistint_len - returns the number of elements
+ * in a double linked list
+ *
  * @h: pointer to the beginning of a linked list
  *
- * Return: number of nodes
+ * Return: the number of nodes
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t i;
+	int count;
 
-	for (i = 0; h != NULL; i++)
+	count = 0;
+
+	if (h == NULL)
+		return (count);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
+	{
+		count++;
 		h = h->next;
-	return (i);
+	}
+
+	return (count);
 }
